bd512fe11852393925a8d71ae80a3059ffe9a16c
akivanc88 <er.akashmajumder@gmail.com>
2022-10-01 15:55:46 -0700

    Connect FE to BE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+async function backendCall(){
+  const response = await axios.get("http://localhost:8080");
+
+  console.log(response.data);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={backendCall}>Fetch Hello</button>
     </div>
   );
 }
